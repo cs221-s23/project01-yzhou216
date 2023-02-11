79e3be4028c6bbff5833ece253d36fda5324863a
Yiyu Zhou <yzhou155@vlab17.cs.usfca.edu>
2023-02-11 00:11:06 -0800

    Clean up

--- project01.c
+++ project01.c
@@ -10,27 +10,27 @@
 
 void sha256(char *dest, char *src)
 {
-    /* zero out the sha256 context */
-    struct sha256_ctx ctx;
-    memset(&ctx, 0, sizeof(ctx));
+	/* zero out the sha256 context */
+	struct sha256_ctx ctx;
+	memset(&ctx, 0, sizeof(ctx));
 
-    /* zero out the binary version of the hash digest */
-    unsigned char dig_bin[DIG_BIN_LEN];
-    memset(dig_bin, 0, DIG_BIN_LEN);
+	/* zero out the binary version of the hash digest */
+	unsigned char dig_bin[DIG_BIN_LEN];
+	memset(dig_bin, 0, DIG_BIN_LEN);
 
-    /* zero out the string version of the hash digest */
-    memset(dest, 0, DIG_STR_LEN);
+	/* zero out the string version of the hash digest */
+	memset(dest, 0, DIG_STR_LEN);
 
-    /* compute the binary hash digest */
-    __sha256_init_ctx(&ctx);
-    __sha256_process_bytes(src, strlen(src), &ctx);
-    __sha256_finish_ctx(&ctx, dig_bin);
+	/* compute the binary hash digest */
+	__sha256_init_ctx(&ctx);
+	__sha256_process_bytes(src, strlen(src), &ctx);
+	__sha256_finish_ctx(&ctx, dig_bin);
 
-    /* convert it into a string of hexadecimal digits */
-    for (int i = 0; i < DIG_BIN_LEN; i++) {
-        snprintf(dest, 3, "%02x", dig_bin[i]);
-        dest += 2;
-    }
+	/* convert it into a string of hexadecimal digits */
+	for (int i = 0; i < DIG_BIN_LEN; i++) {
+		snprintf(dest, 3, "%02x", dig_bin[i]);
+		dest += 2;
+	}
 }
 
 char *leet(char *str)
