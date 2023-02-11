6d31788ecf3c9c61ee82dcba4b2e7895958a56f9
Yiyu Zhou <yzhou155@vlab17.cs.usfca.edu>
2023-02-11 00:07:32 -0800

    Clean up

--- project01.c
+++ project01.c
@@ -10,23 +10,23 @@
 
 void sha256(char *dest, char *src)
 {
-    // zero out the sha256 context
+    /* zero out the sha256 context */
     struct sha256_ctx ctx;
     memset(&ctx, 0, sizeof(ctx));
 
-    // zero out the binary version of the hash digest
+    /* zero out the binary version of the hash digest */
     unsigned char dig_bin[DIG_BIN_LEN];
     memset(dig_bin, 0, DIG_BIN_LEN);
 
-    // zero out the string version of the hash digest
+    /* zero out the string version of the hash digest */
     memset(dest, 0, DIG_STR_LEN);
 
-    // compute the binary hash digest
+    /* compute the binary hash digest */
     __sha256_init_ctx(&ctx);
     __sha256_process_bytes(src, strlen(src), &ctx);
     __sha256_finish_ctx(&ctx, dig_bin);
 
-    // convert it into a string of hexadecimal digits
+    /* convert it into a string of hexadecimal digits */
     for (int i = 0; i < DIG_BIN_LEN; i++) {
         snprintf(dest, 3, "%02x", dig_bin[i]);
         dest += 2;
